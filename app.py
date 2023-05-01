ce75ef68fc36e7dfa6e5ddc8e240d8ac7242cadd
Sandip Deb <debsandip.agt@gmail.com>
2023-05-01 10:36:38 +0530

    Doing some changed in app.py file

--- app.py
+++ app.py
@@ -4,24 +4,24 @@ import numpy as np
 import pandas as pd
 
 app = Flask(__name__)
-svmmodel = pickle.load(open('titanic_model0.1.0.pkl','rb'))
+
+svmmodel = pickle.load(open('titanic_model0.1.0.pkl', 'rb'))
 scaler = pickle.load(open('scaling.pkl', 'rb'))
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api', methods=['POST'])
+@app.route('/predict_api', methods = ['POST'])
 def predict_api():
-    data = request.json['data']
+    data = request.json('data')
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data = scaler.transform(np.array(list(data.values())).reshape(-1,1))
+    print(np.array(list(data.values())).reshape(-1,1))
+    new_data = scaler.transforn(np.array(list(data.values)).reshape(-1,1))
     output = svmmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
-    
-
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port='8080', debug=True)
+    
